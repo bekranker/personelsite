bbea2c4ea50f92cf891783b56238dcaaddeb8f81
Bekir <62219677+bekranker@users.noreply.github.com>
2024-12-27 11:23:35 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,14 +35,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm a Unity Game Developer {" "}
-          <Link
-            target="_blank"
-            href="https://unkey.dev"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            as
-          </Link> I am designing algorithms to create game mechanichs
+          I'm a Unity Game Developer I am designing algorithms to create game mechanichs
         </h2>
       </div>
     </div>
